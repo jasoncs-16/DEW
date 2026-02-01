4bb45082577a5669ba017074e6d152a4e4fdbec8
jasoncs-16 <jasoncsotto16@gmail.com>
2026-02-01 23:45:02 +0000

    register, login, lang, products

--- ProyectoFinal/main.js
+++ ProyectoFinal/main.js
@@ -61,12 +61,12 @@ function showText(idText) {
 // Objeto de todos los botones con texto escondido
 const buttons = {
     aboutButton: "companyInformation",
-    airfryer1: "airfryerInformation",
+    airfryer: "airfryerInformation",
     airfryer2: "airfryerInformation",
     colador: "coladorInformation",
     cuchillo: "cuchilloInformation",
     sarten: "sartenInformation",
-    tabla1: "tablaInformation",
+    tabla: "tablaInformation",
     tabla2: "tablaInformation"
 }
 
@@ -77,13 +77,106 @@ Object.entries(buttons).forEach(([buttonId, textId]) => {
     });
 });
 
+const productButton = document.getElementById("productButton");
+const productMenu = document.getElementById("productMenu");
+
+// Al pulsar el boton Productos de la barra de navegacion, desplegar el menu
+productButton.addEventListener("click", function() {
+    if (productMenu.style.display == "none") { // Apunta directamente al menu desplegable para mostrarlo o no
+        productMenu.style.display = "block"
+    } else {
+        productMenu.style.display = "none"
+    }
+});
+
+// Pasar por cada elemento del menu desplegable y mostrar la infomracion de cada uno
+const productsLi = {
+    lista0: "airfryerInformation",
+    lista1: "coladorInformation",
+    lista2: "cuchilloInformation",
+    lista3: "sartenInformation",
+    lista4: "tablaInformation",
+}
+
+// Recorrer cada boton y llamar a la funcion showText
+Object.entries(productsLi).forEach(([buttonId, textId]) => {
+    document.getElementById(buttonId).addEventListener("click", () => {
+        showText(textId);
+    });
+});
+
+// Evitar que se recargue la pagina
+const formUsuario = document.querySelector("#formUsuario").addEventListener("submit", function(e) {
+    e.preventDefault();
+});
+
+// Expresiones regulares para registrarse
+const validators = {
+    name: /^[A-Z][a-z]+$/,
+    id: /^[0-9]+$/,
+    email: /^[a-zA-Z]*[0-9]*@(gmail|hotmail)(.com|.es)$/,
+    bankAcc: /^[0-9]{16}$/,
+    phoneNumber: /^[0-9]{9}$/,
+    password: /^([a-zA-Z]|[0-9]|\W){12,}$/
+}
+
+function register() {
+    Object.entries(validators).forEach(([clave, valor]) => {
+        let input = document.querySelector("input[name='" + clave + "']");
+        let values = input.value;
+        if (!valor.test(values)) {
+            input.className = "invalido";
+        } else {
+            input.className = "valido";
+        }
+    });
+}
+
+function verifyForm() {
+    for (const clave of Object.keys(validadores)) {
+        let input = document.querySelector("input[name='" + clave + "']");
+        if (input.className == "invalido" || input.className == "") {
+            return false;
+        }
+    }
+    return true;
+}
+
+function saveUser() {
+    register();
+
+    if (verifyForm()) {
+        const datosUsuario = {}
+        for (const clave of Object.keys(validadores)) {
+            let input = document.querySelector("input[name='" + clave + "']");
+            datosUsuario[clave] = input.value;
+        }
+        sessionStorage.setItem("Usuario", JSON.stringify(datosUsuario));
+    }
+}
+
+const registerButton = document.getElementById("userRegister");
+registerButton.addEventListener("click", saveUser)
+
 // Mostrar el carrito
 const shoppingCart = document.getElementById("shoppingCart");
 const shoppingCartSection = document.getElementById("shoppingCartSection");
 shoppingCart.addEventListener("click", function() {
-    if (shoppingCartSection.hidden == false) {
+    if (shoppingCartSection.hidden == false) { // Apunta directamente al carrito para mostrarlo o no
         shoppingCartSection.hidden = true
     } else {
         shoppingCartSection.hidden = false
+    }
+});
+
+const langButton = document.getElementById("langButton");
+const lang = document.getElementById("lang");
+
+// Al pulsar el boton del idioma, saldran los idiomas disponibles
+langButton.addEventListener("click", function() {
+    if (lang.style.display == "none") { // Apunta directamente al menu desplegable para mostrarlo o no
+        lang.style.display = "block"
+    } else {
+        lang.style.display = "none"
     }
 });
